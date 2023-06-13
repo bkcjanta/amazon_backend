55cf6ba040eca95fd27d9b7ae82e486c9b1ce39c
bkcjanta <bhupendrafbook@gmail.com>
2023-06-13 22:30:37 +0530

    amazon backend final update

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser());
 app.use(cors({
-    origin: "*",
+    origin: ["https://amazon-shoping.netlify.app/", "http://localhost:3000"],
     credentials: true,  //access-control-allow-credentials:true  
     optionsSuccessStatus: 200 // This line should be optionsSuccessStatus instead of optionSuccessStatus
 }));
